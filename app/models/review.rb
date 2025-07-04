f0bef8368e1ac52dd3a999d9b163a798ae9b9aad
David Peloso <david.g.peloso@gmail.com>
2025-07-03 21:36:29 -0400

    Added broadcast of review changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  #  Broadcast changes to reviews to "subscribers" of the related turbo_stream - aka a streamable.
+  #  broadcasts_refreshes_to:
+  #   - configures the model to broadcast a “page refresh” on creates, updates, and destroys
+  #   - :product is the streamable, which, oddly enough, means listeners will listen for a 
+  #      turbo_stream_from @product
+  broadcasts_refreshes_to :product
 end
