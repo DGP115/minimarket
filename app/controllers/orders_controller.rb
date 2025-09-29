3d1bf83a8dd62f1a0f158664f48cb33f1853fe6a
David Peloso <david.g.peloso@gmail.com>
2025-09-29 14:28:54 -0300

    Orders Mgt done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: %i[ show destroy ]
 
   # GET /orders or /orders.json
   def index
@@ -30,42 +30,7 @@ class OrdersController < ApplicationController
     @order_lineitems = @order.lineitems.order(created_at: :asc).includes(:product)
   end
 
-  # GET /orders/new
-  def new
-    @order = Order.new
-  end
 
-  # GET /orders/1/edit
-  def edit
-  end
-
-  # POST /orders or /orders.json
-  def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /orders/1 or /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /orders/1 or /orders/1.json
   def destroy
